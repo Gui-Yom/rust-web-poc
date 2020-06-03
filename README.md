0b9366d4a5593edfe96717a022001c11024390d8
LimelioN <memorial.limelion@gmail.com>
2020-06-03 09:28:15 +0200

    Add fibonacci with actix-web

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### rust-web-poc
 Just fiddling a bit with rust web frameworks
 
-Run `cargo run --bin fibo` and visit `http://localhost:8000/?n=5` 
+Run `cargo run --bin fibo --features="rocket"` and visit `http://localhost:8000/?n=5` 
 to show the 5th number of the fibonacci sequence.
 
-Run `cargo run --bin graphql --features="juniper juniper_rocket` and
+Run `cargo run --bin graphql --features="rocket juniper juniper_rocket` and
 visit `https://localhost:8000/` to get the GraphQLi interface.
---
+++ src/fibo_actix.rs
@@ -0,0 +1,29 @@
+use actix_web::{App, HttpServer, Responder, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct FiboReq {
+    n: u32
+}
+
+async fn index(web::Query(req): web::Query<FiboReq>) -> String {
+    format!("{}", fibo(req.n))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/").route("/", web::get().to(index)),
+        )
+    })
+        .bind("127.0.0.1:8088")?
+        .run()
+        .await
+}
+
+const GOLDEN_RATIO: f64 = 1.6180339887;
+
+fn fibo(n: u32) -> u64 {
+    (GOLDEN_RATIO.powi(n as i32) / (5.0_f64).sqrt() as f64 + 0.5).trunc() as u64
+}
